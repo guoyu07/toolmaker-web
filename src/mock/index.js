be482940ed59e06a8e821437ed169b20c2860ab2
Gordon Wang <wxnchcp_cn@sina.com>
2017-10-31 17:32:24 +1100

    move login.js to auth.js

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,5 +1,5 @@
 import Mock from 'mockjs'
-import loginAPI from './login'
+import loginAPI from './auth'
 import userAPI from './user'
 import requirementAPI from './requirement'
 import articleAPI from './article'
@@ -9,20 +9,23 @@ Mock.setup({
   timeout: '350-600'
 })
 
-// 登录相关
+// 权限认证
 Mock.mock(/\/login\/login/, 'post', loginAPI.loginByUsername)
 Mock.mock(/\/login\/logout/, 'post', loginAPI.logout)
 Mock.mock(/\/users\/info\.*/, 'get', loginAPI.getUserInfo)
 
-// User相关
+// User
 Mock.mock(/\/users/, 'get', userAPI.getList)
+
+// Requirement
 Mock.mock(/\/requirements/, 'get', requirementAPI.getList)
 
-// // 文章相关
+// 文章
 Mock.mock(/\/article\/list/, 'get', articleAPI.getList)
 Mock.mock(/\/article\/detail/, 'get', articleAPI.getArticle)
 Mock.mock(/\/article\/pv/, 'get', articleAPI.getPv)
-// // table example相关
+
+// table example
 Mock.mock(/\/article_table\/list/, 'get', article_tableAPI.getList)
 Mock.mock(/\/article_table\/p/, 'get', article_tableAPI.getPv)
 
